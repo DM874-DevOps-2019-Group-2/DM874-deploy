3228a19a453a3fba9260742e9d4e365cf6523501
Henrik Sejer Pedersen <henrik@opgo.dk>
2019-12-02 17:40:08 +0100

    update docker_push

--- docker_push.sh
+++ docker_push.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 if [ -z "$DOCKER_USERNAME" ]
 then
-	>&2 echo "Docker username not provided"
-	exit 126
+	>&2 echo "DOCKER_USERNAME not provided"
+	exit 22
 fi
 
 if [ -z "$DOCKER_PASSWORD" ]
 then
-	>&2 echo "Docker password not provided"
-	exit 126
+	>&2 echo "DOCKER_PASSWORD not provided"
+	exit 22
 fi
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
